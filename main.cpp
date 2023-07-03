76157838e297f74cec58b2112a053d05a53a101b
max <maksim.levental@gmail.com>
2023-07-02 19:37:43 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "eval_frame.h"
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
---
+++ main.py
@@ -0,0 +1,3 @@
+import inspect
+
+print(inspect.currentframe().f_code.co_name)
