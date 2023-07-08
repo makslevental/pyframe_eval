798c71229463f61d3adb1cb39673573344ad3406
max <maksim.levental@gmail.com>
2023-07-08 15:57:44 -0500

    doesn't work

--- pyframe_eval/__init__.py
+++ pyframe_eval/__init__.py
@@ -12,6 +12,11 @@ from ._pyframe_eval import (
 
 def copy_func(f, new_code):
     """Based on http://stackoverflow.com/a/6528148/190597 (Glenn Maynard)"""
+    new_code = new_code.replace(
+        co_name=new_code.co_name + "__updated",
+        co_freevars=f.__code__.co_freevars,
+        co_cellvars=f.__code__.co_cellvars,
+    )
     g = types.FunctionType(
         new_code,
         f.__globals__,
@@ -25,16 +30,44 @@ def copy_func(f, new_code):
     return g
 
 
+def _is_wrapper(f):
+    return hasattr(f, "__wrapped__")
+
+
 def eval_custom_code(new_code, frame):
-    new_code = new_code.replace(co_name=new_code.co_name + "__updated")
-    updated_f = copy_func(frame.f_func, new_code)
     arg_info = inspect.getargvalues(frame)
+    closure = inspect.getclosurevars(frame.f_func)
+    updated_f = copy_func(frame.f_func, new_code)
+    # print(arg_info)
+    # print(f"{arg_info=}")
     localsplus = dict(zip(frame.localsplusnames, frame.localsplus))
-    posargs = [localsplus[a] for a in arg_info.args]
+    # print(localsplus)
+    # print(f"{localsplus=}")
+    args = {a: localsplus[a] for a in arg_info.args}
+    # print(f"{args=}")
     varargs = localsplus[arg_info.varargs] if arg_info.varargs is not None else []
     kwargs = localsplus[arg_info.keywords] if arg_info.keywords is not None else {}
+    # print(f"{kwargs=}")
+    sig = inspect.signature(updated_f)
+    params = list(sig.parameters.items())
+    var_args_first = (
+        params[0][1].kind == inspect.Parameter.VAR_POSITIONAL if len(params) else False
+    )
+    # print(frame.f_func.__name__, frame.f_func.__module__)
+    print(args, varargs, kwargs)
+    print(sig)
+    if var_args_first:
+        bound_args = sig.bind(*varargs, **args, **kwargs)
+        bound_args.apply_defaults()
+    else:
+        bound_args = sig.bind(*args.values(), *varargs, **kwargs)
+        bound_args.apply_defaults()
+
+    print(bound_args)
+    args, kwargs = bound_args.args, bound_args.kwargs
+    print(args, kwargs)
     enable_eval_frame(None, True)
-    return updated_f(*posargs, *varargs, **kwargs)
+    return updated_f(*args, **kwargs)
 
 
 class Dynamo:
@@ -47,10 +80,11 @@ class Dynamo:
 
     def __enter__(self):
         set_rewrite_code_callback(None, self.callback)
-        set_eval_custom_code_callback(None, eval_custom_code)
+        # set_eval_custom_code_callback(None, eval_custom_code)
         enable_eval_frame(None, True)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         set_rewrite_code_callback(None, None)
         set_eval_custom_code_callback(None, None)
         enable_eval_frame(None, False)
+
--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,11 @@
 import ast
+import functools
 import inspect
-import io
-from contextlib import redirect_stdout
 from textwrap import dedent
 from types import CodeType
 from typing import Any
+
+import numpy as np
 
 import pyframe_eval
 
@@ -60,6 +61,7 @@ with pyframe_eval.Dynamo(smoke_test_callback, skips=["__exit__"]):
     r = foo(1)
     assert r == 16
 
+
 # assert f.getvalue() == dedent(
 #     f"""\
 # {Foo.__call__.__name__} {Foo.__call__.__code__.co_filename} {Foo.__call__.__code__.co_firstlineno}
@@ -74,26 +76,36 @@ class ScaleConstants(ast.NodeTransformer):
         self.s = s
 
     def visit_Constant(self, node: ast.Constant) -> Any:
-        if node.value:
-            node.value *= self.s
+        # if node.value and isinstance(node.value, int):
+        #     node.value *= self.s
         return node
+
+
+def _is_wrapper(f):
+    return hasattr(f, "__wrapped__")
 
 
 def rewrite_callback(frame):
     fun = frame.f_func
+    if _is_wrapper(fun) or inspect.ismethodwrapper(fun):
+        return None
+
     tree = ast.parse(dedent(inspect.getsource(fun)))
     assert isinstance(
         tree.body[0], ast.FunctionDef
     ), f"unexpected ast node {tree.body[0]}"
-    tree = ScaleConstants(s=10).visit(tree)
+    tree = ScaleConstants(s=1).visit(tree)
     tree = ast.fix_missing_locations(tree)
     tree = ast.increment_lineno(tree, fun.__code__.co_firstlineno - 1)
     module_code_o = compile(tree, fun.__code__.co_filename, "exec")
-    f_code_o = next(
-        c
-        for c in module_code_o.co_consts
-        if type(c) is CodeType and c.co_name == fun.__name__
-    )
+    try:
+        f_code_o = next(
+            c
+            for c in module_code_o.co_consts
+            if type(c) is CodeType and c.co_name == fun.__name__
+        )
+    except StopIteration:
+        f_code_o = fun.__code__
     return f_code_o
 
 
@@ -168,3 +180,50 @@ def hasboth(*args, **kwargs):
 hasboth(1, 2, 3, a=4, b=5, c=6)
 with pyframe_eval.Dynamo(rewrite_callback):
     hasboth(1, 2, 3, a=4, b=5, c=6)
+
+
+def hasdefaultsandoverrides(a=1, b=2, c=3):
+    print("hasdefs", a, b, c)
+
+
+hasdefaultsandoverrides(b=5, c=6)
+with pyframe_eval.Dynamo(rewrite_callback):
+    hasdefaultsandoverrides(b=5, c=6)
+
+
+def _np_wrapper(*args, factory=None, **kwargs):
+    return factory(*args, **kwargs)
+
+
+def star_args_wrapper(factory):
+    @functools.wraps(factory)
+    def wrapper(*args, **kwargs):
+        if len(args) == 1 and isinstance(args[0], (list, tuple)):
+            # Input is a list or tuple
+            return factory(args[0], **kwargs)
+        else:
+            # Input is a sequence of ints along with kwargs
+            return factory(args, **kwargs)
+
+    return wrapper
+
+
+def standard_normal(*args, **kwargs):
+    rng = np.random.default_rng()
+
+    return np.random.Generator.standard_normal(rng, *args, **kwargs)
+
+
+empty_placeholder = functools.partial(_np_wrapper, factory=np.empty)
+ones = functools.partial(_np_wrapper, factory=star_args_wrapper(np.ones))
+zeros = functools.partial(_np_wrapper, factory=star_args_wrapper(np.zeros))
+rand = functools.partial(_np_wrapper, factory=np.random.rand)
+randn = functools.partial(_np_wrapper, factory=star_args_wrapper(standard_normal))
+tensor = functools.partial(_np_wrapper, factory=np.array)
+
+
+print(ones(1, 2, 3))
+print(ones((1, 2, 3)))
+with pyframe_eval.Dynamo(rewrite_callback):
+    print(ones(1, 2, 3))
+    print(ones((1, 2, 3)))
